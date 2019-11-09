b5cc4ac0cd0c627f55df01d161b160750d5ed14e
Alexander Sokol <sokol@dualitygroup.com>
2019-11-09 09:24:29 -0500

    Corrected linter errors in Data and Key classes, added copyright.

--- py/datacentric/record/key.py
+++ py/datacentric/record/key.py
@@ -1,11 +1,24 @@
+# Copyright (C) 2013-present The DataCentric Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import datetime as dt
 from abc import ABC
 from enum import Enum
 from typing import List
 from bson import ObjectId
-
-from datacentric.date_time.local_minute import LocalMinute
 from datacentric.record.data import Data
+from datacentric.date_time.local_minute import LocalMinute
 import datacentric.date_time.date_ext as date_ext
 
 
@@ -14,7 +27,8 @@ class Key(Data, ABC):
     type specific become key tokens. Property value and str(self)
     consists of key tokens with semicolon delimiter.
     """
-    __slots__ = []
+
+    __slots__: List = []
 
     def __init__(self):
         super().__init__()
@@ -151,7 +165,7 @@ class Key(Data, ABC):
                 raise Exception(f'Key {";".join(tokens)} for key type {type(self).__name__} contains an empty token.')
 
             if member_type == str:
-                value = token
+                value: object = token
             elif member_type == bool:
                 value = bool(token.capitalize())
             elif member_type == int:
