b7f7e369e13d29c7088a1fd7688458d603f2ab07
Vitalii Kanishchev <vitalyk@dualitygroup.com>
2019-11-21 18:24:41 +0100

    Applied attrs to Data and fixed class attributes.

--- py/datacentric/storage/data.py
+++ py/datacentric/storage/data.py
@@ -12,21 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import attr
 from abc import ABC
-from typing import List
+from typing import Tuple
 from datacentric.attributes.class_attribute import ClassAttribute
 
+
+@attr.s(slots=True)
 class Data(ABC):
     """ Abstract base class for data structures"""
 
-    __slots__ = ()
-
-    class_attributes: List[ClassAttribute] = []
+    class_attributes: Tuple[ClassAttribute, ...] = None
     """
     Class attributes are assigned to the class via static element
     class_attributes which by default is empty. The user can then
     iterate over or search in this list to find the desired attribute.
     """
-
-    def __init__(self):
-        pass
