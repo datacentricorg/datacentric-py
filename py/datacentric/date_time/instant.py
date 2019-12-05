1894071ea20e276ee5b415102651502c25a3f6eb
Alexander Sokol <sokol@dualitygroup.com>
2019-12-05 11:22:00 -0500

    DC-36 Updated Instant to the new approach.

--- py/datacentric/date_time/instant.py
+++ py/datacentric/date_time/instant.py
@@ -18,7 +18,7 @@ import datetime as dt
 import pytz
 
 
-class Instant(int, ABC):
+class Instant(dt.datetime, ABC):
     """
     Represents an instant on the global timeline in UTC timezone, with
     one millisecond resolution, and provides conversion to and from:
@@ -44,14 +44,15 @@ class Instant(int, ABC):
 
     # --- STATIC
 
-    def __init__(self,
-                 year_or_value: Union[int, str, dt.datetime],
-                 month: Optional[int] = None,
-                 day: Optional[int] = None,
-                 hour: Optional[int] = None,
-                 minute: Optional[int] = None,
-                 second: Optional[int] = None,
-                 millisecond: Optional[int] = None):
+    @classmethod
+    def from_fields(cls,
+                    year: int,
+                    month: int,
+                    day: int,
+                    hour: int,
+                    minute: int,
+                    second: int,
+                    millisecond: Optional[int] = None) -> Union[dt.datetime, 'Instant']:
         """
         Creates Instant to millisecon precision from the specified
         arguments. The options for the arguments include:
@@ -63,170 +64,200 @@ class Instant(int, ABC):
         * Pandas pd.timestamp
         """
 
-        __unix_millis = None
-        """Milliseconds since Unix epoch."""
+        # If millisecond is not specified, assume 0
+        if millisecond is None:
+            millisecond = 0
 
-        # Becuase Python has no method overloads, we have to use default ctor
-        # arguments and determine the type dynamically. This flag is true
-        # if first argument is used to pass the entire value, in which case
-        # all other arguments (month, etc.) must be None
-        remaining_args_must_be_none: bool = True
+        # Create dt.datetime in UTC timezone
+        result: dt.datetime = dt.datetime(year, month, day, hour, minute, second, 1000 * millisecond, pytz.UTC)
+        return result
 
-        # Determine what is passed as first ctor argument
-        if isinstance(year_or_value, int):
+    @classmethod
+    def from_str(cls, value: str) -> Union[dt.datetime, 'Instant']:
+        """
+        Convert from string in ISO format with UTC (Z) timezone suffix
+        and up to 7 digits after decimal points for seconds.
 
-            # If first argument is int, it may be year or Unix epoch millis
-            year_or_unix_millis: int = year_or_value
+        This method rounds the result to whole milliseconds.
 
-            if 1970 <= year_or_unix_millis <= 9999:
+        Examples:
 
-                # First argument is year if in the range from 1970 to 9999 (inclusive)
-                year: int = year_or_unix_millis
+        2003-05-01T10:15:30Z
+        2003-05-01T10:15:30.1Z
+        ...
+        2003-05-01T10:15:30.1234567Z
+        """
 
-                # This is one type of input where remaning args are used
-                remaining_args_must_be_none = False
+        if not value.endswith('Z'):
+            raise Exception(f'String {value} passed to Instant.from_str(...) method '
+                            f'must end with capital Z that indicates UTC timezone.')
 
-                # If the first argument is year, only millisecond is optional
-                if month is None:
-                    raise Exception(f'In Instant constructor, first argument {year} is year '
-                                    f'in which case month must be specified.')
-                if day is None:
-                    raise Exception(f'In Instant constructor, first argument {year} is year '
-                                    f'in which case day must be specified.')
-                if hour is None:
-                    raise Exception(f'In Instant constructor, first argument {year} is year '
-                                    f'in which case hour must be specified.')
-                if minute is None:
-                    raise Exception(f'In Instant constructor, first argument {year} is year '
-                                    f'in which case minute must be specified.')
-                if second is None:
-                    raise Exception(f'In Instant constructor, first argument {year} is year '
-                                    f'in which case second must be specified.')
-
-                # If millisecond is not specified, assume 0
-                if millisecond is None:
-                    millisecond = 0
-
-                # Create dt.datetime in UTC timezone
-                value: dt.datetime = dt.datetime(year, month, day, hour, minute, second, 1000 * millisecond,
-                                                 pytz.UTC)
-
-                # Round to the nearest whole milliseconds since Unix epoch
-                self.__unix_millis = round((value - self.__unix_epoch).total_seconds() * 1000)
-
-            elif year_or_unix_millis >= 0:
-
-                # Otherwise first argument is milliseconds since Unix epoch
-                self.__unix_millis = year_or_unix_millis
-
-            else:
-                raise Exception(f'First argument of Instant constructor {year_or_unix_millis} is negative.')
-
-        elif isinstance(year_or_value, str):
-
-            # If argument is a string, it must specify UTC (Z) timezone explicitly
-            iso_string: str = year_or_value
-            if not iso_string.endswith('Z'):
-                raise Exception(f'String {iso_string} passed to Instant ctor must end with capital Z that '
-                                f'indicates UTC timezone.')
-
-            # Convert to datetime and set UTC timezone
-            dtime_from_str: dt.datetime
-            if '.' in iso_string:
-                # Has milliseconds
-                dtime_from_str = dt.datetime.strptime(iso_string, '%Y-%m-%dT%H:%M:%S.%fZ')
-            else:
-                # Does not have milliseconds
-                dtime_from_str = dt.datetime.strptime(iso_string, '%Y-%m-%dT%H:%M:%SZ')
-
-            # Set tzinfo to the standard UTC singleton from pytz module
-            dtime_from_str = dtime_from_str.replace(tzinfo=pytz.UTC)
-
-            # Round to the nearest whole milliseconds since Unix epoch
-            self.__unix_millis = round((dtime_from_str - self.__unix_epoch).total_seconds() * 1000)
-
-        elif isinstance(year_or_value, dt.datetime):
-
-            # First argument is dt.datetime or pd.Timestamp
-            dtime: dt.datetime = year_or_value
-
-            # Check that timezone is UTC, error message otherwise
-            if dtime.utcoffset().microseconds != 0:
-                raise Exception(f'Datetime {dtime} passed to Instant ctor is not in UTC timezone.')
-
-            # Round to the nearest whole milliseconds since Unix epoch.
-            #
-            # Because:
-            #
-            # * Time zone info (tzinfo) is abstract class in Python
-            # * There are several different singletons representing UTC, and
-            # * Comparison will will fail if different instances are passed,
-            #
-            # we must use the instance of tzinfo from the external object here,
-            # which was already verified to have 0 offset relative to UTC
-            unix_epoch: dt.datetime = dt.datetime.fromtimestamp(0, dtime.tzinfo)
-            self.__unix_millis = round((dtime - unix_epoch).total_seconds() * 1000)
-
+        # Convert to datetime and set UTC timezone
+        dtime_from_str: dt.datetime
+        if '.' in value:
+            # Has milliseconds
+            dtime_from_str = dt.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%fZ')
         else:
-            raise Exception(f'First argument of Instant constructor {year_or_value} must be one of '
-                            f'(a) year, (b) milliseconds since Unix epoch, (c) dt.datetime, or (d) pd.Timestamp.')
+            # Does not have milliseconds
+            dtime_from_str = dt.datetime.strptime(value, '%Y-%m-%dT%H:%M:%SZ')
 
-        # Depending on what is passed as the first argument, validate the remaining arguments
-        if remaining_args_must_be_none:
+        # Set tzinfo to the standard UTC singleton from pytz module
+        result: dt.datetime = dtime_from_str.replace(tzinfo=pytz.UTC)
 
-            # If the first argument is not year, the remaining arguments must be None
-            if month is not None:
-                raise Exception(f'In Instant constructor, first argument {year_or_value} is the entire datetime '
-                                f'rather than year, in which case month={month} must be None.')
-            if day is not None:
-                raise Exception(f'In Instant constructor, first argument {year_or_value} is the entire datetime '
-                                f'rather than year, in which case day={day} must be None.')
-            if hour is not None:
-                raise Exception(f'In Instant constructor, first argument {year_or_value} is the entire datetime '
-                                f'rather than year, in which case hour={hour} must be None.')
-            if minute is not None:
-                raise Exception(f'In Instant constructor, first argument {year_or_value} is the entire datetime '
-                                f'rather than year, in which case minute={minute} must be None.')
-            if second is not None:
-                raise Exception(f'In Instant constructor, first argument {year_or_value} is the entire datetime '
-                                f'rather than year, in which case second={second} must be None.')
-            if millisecond is not None:
-                raise Exception(f'In Instant constructor, first argument {year_or_value} is the entire datetime '
-                                f'rather than year, in which case millisecond={millisecond} must be None.')
+        # Round the result to whole milliseconds
+        result = cls.round(result)
+        return result
 
-    def __str__(self) -> str:
+    @classmethod
+    def to_str(cls, value: Union[dt.datetime, 'Instant']) -> str:
         """
         Convert to string in ISO format with UTC (Z) timezone suffix
-        to millisecond precision, for example:
+        and 3 digits after decimal points for seconds, irrespective of
+        how many digits are actually required.
 
-        2003-05-01T10:15:30.5Z
+        Example:
 
-        Fractional seconds are omitted if zero.
+        2003-05-01T10:15:30.500Z
         """
-        dtime: dt.datetime = self.to_datetime()
-        second_str: str = dtime.strftime('%Y-%m-%dT%H:%M:%S')
-        if dtime.microsecond == 0:
-            # No milliseconds
-            return second_str + 'Z'
-        else:
-            # Include milliseconds without the trailing zeroes
-            millis_str: str = str(dtime.microsecond / 1.0e6).lstrip('0.')
-            result: str = second_str + '.' + millis_str + 'Z'
-            return result
 
-    def to_unix_millis(self) -> int:
-        """
-        Convert to the number of milliseconds since Unix epoch.
+        # Ensure the argument is in UTC and does not have fractional milliseconds
+        cls.validate(value)
 
-        Because Instant is rounded to the nearest millisecond,
-        the returned value is always int.
-        """
-        return self.__unix_millis
+        # Convert to string in ISO format with UTC (Z) timezone suffix
+        # and 3 digits after decimal points for seconds, irrespective of
+        # how many digits are actually required.
+        result_to_microseconds: str = value.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        result: str = result_to_microseconds[:-3] + 'Z'
+        return result
 
-    def to_datetime(self) -> dt.datetime:
+    @classmethod
+    def from_unix_millis(cls, unix_millis: int) -> Union[dt.datetime, 'Instant']:
         """
-        Convert to dt.datetime in UTC timezone.
+        Convert milliseconds since Unix epoch Instant stored as dt.datetime
+        in UTC timezone.
         """
         # Convert milliseconds from Unix epoch to seconds, then construct the date
         # and replace timezone with pytz.UTC which is more standard
-        return dt.datetime.utcfromtimestamp(self.__unix_millis / 1000.0).replace(tzinfo=pytz.UTC)
+        return dt.datetime.utcfromtimestamp(unix_millis / 1000.0).replace(tzinfo=pytz.UTC)
+
+    @classmethod
+    def to_unix_millis(cls, value: Union[dt.datetime, 'Instant']) -> int:
+        """
+        Convert Instant stored as dt.datetime in UTC timezone to
+        milliseconds since Unix epoch, rounding the value to
+        whole milliseconds.
+
+        Error message if the argument has fractional milliseconds,
+        use Instant.round(value) to remove.
+        """
+
+        # Ensure the argument is in UTC and does not have fractional milliseconds
+        cls.validate(value)
+
+        # Convert to milliseconds since Unix epoch and round to whole
+        # milliseconds. Because we cannot subtract datetimes with two
+        # different instances of tzinfo, even if both are UTC, we
+        # cannot use a class variabe for unix_epoch. It has to be
+        # created in the argument timezone instead.
+        unix_epoch: dt.datetime = dt.datetime.fromtimestamp(0, value.tzinfo)
+        result: int = round((value - unix_epoch).total_seconds() * 1000)
+        return result
+
+    @classmethod
+    def round(cls, value: Union[dt.datetime, 'Instant']) -> Union[dt.datetime, 'Instant']:
+        """
+        Round the argument to whole milliseconds.
+
+        This method checks that the argument is in UTC.
+        """
+
+        if not cls.is_in_utc(value):
+            raise Exception(f'Instant {cls.__to_str_lenient(value)} is not in UTC. Valid '
+                            f'To avoid timezone uncertainty, Instant values must have '
+                            f'tzinfo=pytz.UTC, or any other that resolves to zero offset '
+                            f'to UTC to avoid timezone conversion uncertainty.')
+
+        rounded_microsecond: int = round(value.microsecond / 1000.0) * 1000
+
+        result: dt.datetime = dt.datetime(
+            value.year,
+            value.month,
+            value.day,
+            value.hour,
+            value.minute,
+            value.second,
+            rounded_microsecond,
+            value.tzinfo
+        )
+        return result
+
+    @classmethod
+    def validate(cls, value: Union[dt.datetime, 'Instant']) -> None:
+        """
+        Raise exception if one of the following is true:
+
+        * Instant has timezone other than UTC
+        * Instant has no timezone
+        * Instant has fractional milliseconds (use Instant.round(...) to remove)
+        """
+        if not cls.is_in_utc(value):
+            raise Exception(f'Instant {cls.__to_str_lenient(value)} is not in UTC. Valid '
+                            f'To avoid timezone uncertainty, Instant values must have '
+                            f'tzinfo=pytz.UTC, or any other that resolves to zero offset '
+                            f'to UTC to avoid timezone conversion uncertainty.')
+
+        if not cls.is_whole_millis(value):
+            raise Exception(f'Instant {cls.__to_str_lenient(value)} has fractional milliseconds. Valid'
+                            f'Instant values must have whole milliseconds to ensure that no loss of precision '
+                            f'occurs during serialization roundtrip. Use Instant.round(...) to remove.')
+
+    @classmethod
+    def is_in_utc(cls, value: Union[dt.datetime, 'Instant']) -> bool:
+        """
+        Return true if timezone is specified and has zero offset to UTC.
+
+        This method does not check if the argument has fractional Unix
+        milliseconds.
+        """
+
+        # Because:
+        #
+        # * Time zone info (tzinfo) is abstract class in Python
+        # * There are several different singletons representing UTC, and
+        # * Comparison will will fail if different instances are passed,
+        #
+        # we must use UTC offset instead of comparing timezone value.
+        result: bool = value.utcoffset().microseconds == 0
+        return result
+
+    @classmethod
+    def is_whole_millis(cls, value: Union[dt.datetime, 'Instant']) -> bool:
+        """
+        Return true if Instant stored as dt.datetime in UTC timezone
+        does not have fractional milliseconds.
+
+        This method does not check if the argument is in UTC.
+        """
+
+        # Convert to milliseconds since Unix epoch and round to whole
+        # milliseconds
+        unix_epoch: dt.datetime = dt.datetime.fromtimestamp(0, value.tzinfo)
+        unix_millis: float = (value - unix_epoch).total_seconds() * 1000
+        rounded_unix_millis: int = round(unix_millis)
+
+        # Return false if more than roundoff tolerance
+        result: bool = abs(unix_millis - rounded_unix_millis) < 1e-10
+        return result
+
+    @classmethod
+    def __to_str_lenient(cls, value: Union[dt.datetime, 'Instant']) -> str:
+        """
+        Performs no validation before convetring to string. Use for error
+        messages only.
+        """
+
+        # Lenient conversion to string without controlling
+        # for timezone or precision. Use for error messages
+        # only.
+        result: str = value.strftime('%Y-%m-%dT%H:%M:%S.%f%Z')
+        return result
--- py/datacentric/test/date_time/test_instant.py
+++ py/datacentric/test/date_time/test_instant.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import unittest
+from typing import Union
 import datetime as dt
 import pandas as pd
 import dateutil
@@ -34,65 +35,50 @@ class TestInstant(unittest.TestCase, UnitTest):
         """Smoke test"""
 
         # Created dates t1-t5 must match this value of string and/or Unix millis
-        date_str: str = '2003-05-01T10:15:30.5Z'
+        date_str: str = '2003-05-01T10:15:30.500Z'
         unix_millis: int = 1051784130500
 
         # Create from milliseconds since Unix epoch
-        t1: Instant = Instant(unix_millis)
-        self.assertEqual(t1.to_unix_millis(), unix_millis)
+        t1: Union[dt.datetime, Instant] = Instant.from_unix_millis(unix_millis)
+        self.assertEqual(Instant.to_unix_millis(t1), unix_millis)
 
         # Create from year, month, etc.
-        t2: Instant = Instant(2003, 5, 1, 10, 15, 30, 500)
-        self.assertEqual(t2.to_unix_millis(), unix_millis)
+        t2: Union[dt.datetime, Instant] = Instant.from_fields(2003, 5, 1, 10, 15, 30, 500)
+        self.assertEqual(Instant.to_unix_millis(t2), unix_millis)
 
         # Create from string
-        t3: Instant = Instant(date_str)
-        self.assertEqual(t3.to_unix_millis(), unix_millis)
+        t3: Union[dt.datetime, Instant] = Instant.from_str(date_str)
+        self.assertEqual(Instant.to_unix_millis(t3), unix_millis)
 
-        # Create from dt.datetime
+        # Create from dt.datetime constructed externally
         dtime: dt.datetime = dateutil.parser.parse(date_str)
-        t4: Instant = Instant(dtime)
-        self.assertEqual(t4.to_unix_millis(), unix_millis)
+        t4: Union[dt.datetime, Instant] = dtime
+        self.assertEqual(Instant.to_unix_millis(t4), unix_millis)
 
-        # Create from pd.timestamp
+        # Create from pd.timestamp constructed externally
         tstamp: pd.Timestamp = pd.Timestamp(date_str)
-        t5: Instant = Instant(tstamp)
-        self.assertEqual(t5.to_unix_millis(), unix_millis)
+        t5: Union[dt.datetime, Instant] = tstamp.to_pydatetime()
+        self.assertEqual(Instant.to_unix_millis(t5), unix_millis)
 
-        # Test conversion to dt.datetime
-        self.assertEqual(t1.to_datetime(), dtime)
-
-        # Test string representation roundtrip
-        self.assertEqual(str(t1), date_str)
+        # Test string representation conversion
+        self.assertEqual(Instant.to_str(t1), date_str)
         t6_str = '2003-05-01T10:15:30Z'
-        self.assertEqual(str(Instant(t6_str)), t6_str)
+        t6_str_result = '2003-05-01T10:15:30.000Z'
+        self.assertEqual(Instant.to_str(Instant.from_str(t6_str)), t6_str_result)
         t7_str = '2003-05-01T10:15:30.1Z'
-        self.assertEqual(str(Instant(t7_str)), t7_str)
+        t7_str_result = '2003-05-01T10:15:30.100Z'
+        self.assertEqual(Instant.to_str(Instant.from_str(t7_str)), t7_str_result)
         t8_str = '2003-05-01T10:15:30.12Z'
-        self.assertEqual(str(Instant(t8_str)), t8_str)
+        t8_str_result = '2003-05-01T10:15:30.120Z'
+        self.assertEqual(Instant.to_str(Instant.from_str(t8_str)), t8_str_result)
         t9_str = '2003-05-01T10:15:30.123Z'
-        self.assertEqual(str(Instant(t9_str)), t9_str)
+        t9_str_result = '2003-05-01T10:15:30.123Z'
+        self.assertEqual(Instant.to_str(Instant.from_str(t9_str)), t9_str_result)
 
         # Test rounding to the whole millisecond
         t10_str = '2003-05-01T10:15:30.1234Z'
         t10_str_rounded = '2003-05-01T10:15:30.123Z'
-        self.assertEqual(str(Instant(t10_str)), t10_str_rounded)
-
-        # Check comparison operators
-        t11a = Instant('2003-05-01T10:15:30.1Z')
-        t11a_new_instance = Instant('2003-05-01T10:15:30.1Z')
-        t11b = Instant('2003-05-01T10:15:30.12Z')
-        self.assertTrue(t11a == t11a)
-        self.assertTrue(t11a == t11a_new_instance)
-        self.assertTrue(t11a != t11b)
-        self.assertTrue(t11a <= t11a)
-        self.assertTrue(t11a <= t11a_new_instance)
-        self.assertTrue(t11a >= t11a)
-        self.assertTrue(t11a >= t11a_new_instance)
-        self.assertTrue(t11a < t11b)
-        self.assertTrue(t11a <= t11b)
-        self.assertTrue(t11b > t11a)
-        self.assertTrue(t11b >= t11a)
+        self.assertEqual(Instant.to_str(Instant.from_str(t10_str)), t10_str_rounded)
 
 
 if __name__ == "__main__":
