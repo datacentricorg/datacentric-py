26a8ca69eb5f1f618965e959a4974e5fe61e1fbc
Alexander Sokol <sokol@dualitygroup.com>
2019-12-06 21:44:37 -0500

    Added get_tzinfo_from_key method to Zone record.
    (cherry picked from commit 1fec4048c467b490fd30fb4fc96217d4b0ea9c06)

--- py/datacentric/date_time/zone.py
+++ py/datacentric/date_time/zone.py
@@ -14,6 +14,8 @@
 
 import attr
 from typing import Union
+import datetime as dt
+import pytz
 from datacentric.storage.record import Record
 from datacentric.date_time.zone_key import ZoneKey
 
@@ -62,6 +64,8 @@ class Zone(Record):
     country and the other the city, for example America/New_York.
     """
 
+    # --- METHODS
+
     def to_key(self) -> str:
         """Get Zone key."""
         return 'Zone=' + self.zone_name
@@ -70,3 +74,16 @@ class Zone(Record):
     def create_key(cls, *, zone_name: str) -> Union[str, ZoneKey]:
         """Create Zone key."""
         return 'Zone=' + zone_name
+
+    # --- STATIC
+
+    @classmethod
+    def get_zone_name_from_key(cls, key: Union[str, ZoneKey]) -> str:
+        """Get zone_name by parsing key."""
+        return key.split('=', 1)[1].split(';')[0]
+
+    @classmethod
+    def get_tzinfo_from_key(cls, key: Union[str, ZoneKey]) -> dt.tzinfo:
+        """Get tzinfo object by parsing key, without loading the zone record."""
+        zone_name: str = cls.get_zone_name_from_key(key)
+        return pytz.timezone(zone_name)
