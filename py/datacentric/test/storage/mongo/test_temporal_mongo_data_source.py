ae3feec9cb84ac7a7fe8367601c4b4a1a4835a31
Alexander Sokol <sokol@dualitygroup.com>
2019-12-08 23:17:20 -0500

    Cleaned up unit test for the temporal Mongo data source.
    (cherry picked from commit 6510b46ba29e94d392deb574cdae751353d3ae3c)

--- py/datacentric/test/storage/mongo/test_temporal_mongo_data_source.py
+++ py/datacentric/test/storage/mongo/test_temporal_mongo_data_source.py
@@ -22,99 +22,9 @@ from datacentric.date_time.local_minute import LocalMinute
 from datacentric.date_time.local_date_time import LocalDateTime
 from datacentric.test.storage.enum_sample import EnumSample
 from datacentric.test.storage.element_sample import ElementSample
-from datacentric.test.storage.base_sample import BaseSample, BaseSampleKey
+from datacentric.test.storage.base_sample import BaseSample
 from datacentric.test.storage.derived_sample import DerivedSample
 from datacentric.storage.mongo.temporal_mongo_unit_test_context import TemporalMongoUnitTestContext
-
-
-def save_base_record(context: Context, data_set_id, record_id, record_index) -> ObjectId:
-    rec = BaseSample()
-    rec.record_name = record_id
-    rec.record_index = record_index
-    rec.double_element = 100.0
-    rec.local_date_element = LocalDate.from_fields(2003, 5, 1)
-    rec.local_time_element = LocalTime.from_fields(10, 15, 30)  # 10:15:30
-    rec.local_minute_element = LocalMinute.from_fields(10, 15)  # 10:15
-    rec.local_date_time_element = LocalDateTime.from_fields(2003, 5, 1, 10, 15)  # 2003-05-01T10:15:00
-    rec.enum_value = EnumSample.EnumValue2
-
-    data_set = context.data_source.get_data_set(data_set_id, context.data_set)
-    context.data_source.save_one(BaseSample, rec, data_set)
-
-    return rec.id_
-
-
-def save_derived_record(context, data_set_id, record_id, record_index) -> ObjectId:
-    rec = DerivedSample()
-    rec.record_name = record_id
-    rec.record_index = record_index
-    rec.double_element = 300.
-    rec.local_date_element = LocalDate.from_fields(2003, 5, 1)
-    rec.local_time_element = LocalTime.from_fields(10, 15, 30)  # 10:15:30
-    rec.local_minute_element = LocalMinute.from_fields(10, 15)  # 10:15
-    rec.local_date_time_element = LocalDateTime.from_fields(2003, 5, 1, 10, 15)  # 2003-05-01T10:15:00
-    rec.string_element2 = ''
-    rec.double_element = 200.
-    rec.list_of_string = ['A', 'B', 'C']
-
-    rec.list_of_double = [1.0, 2.0, 3.0]
-    rec.list_of_nullable_double = [10.0, None, 30.0]
-
-    # Data element
-    rec.data_element = ElementSample()
-    rec.data_element.double_element3 = 1.0
-    rec.data_element.string_element3 = 'AA'
-
-    # Data element list
-
-    element_list0 = ElementSample()
-    element_list0.double_element3 = 1.0
-    element_list0.string_element3 = "A0"
-    element_list1 = ElementSample()
-    element_list1.double_element3 = 2.0
-    element_list1.string_element3 = "A1"
-    rec.data_element_list = [element_list0, element_list1]
-
-    # Key element
-    rec.key_element = 'BB;2'
-
-    # Key element list
-    rec.key_element_list = ['B0;3', 'B1;4']
-
-    data_set = context.data_source.get_data_set(data_set_id, context.data_set)
-    context.data_source.save_one(DerivedSample, rec, data_set)
-    return rec.id_
-
-
-def save_basic_data(context: Context):
-    data_set0 = context.data_source.create_data_set('DataSet0', context.data_set)
-    save_base_record(context, 'DataSet0', 'A', 0)
-    context.data_source.create_data_set('DataSet1', context.data_set, [data_set0])
-    save_derived_record(context, 'DataSet1', 'B', 0)
-
-
-def verify_load(context, data_set_id, key):
-    data_set = context.data_source.get_data_set(data_set_id, context.data_set)
-    record = context.data_source.load_or_null_by_key(BaseSample, key, data_set)
-    if record is None:
-        return 'Not found'
-    else:
-        if record.to_key() != key:
-            return 'Found, key mismatch.'
-        else:
-            return f'Found, type = {type(record).__name__}'
-
-
-def save_minimal_record(context, data_set_id, record_id, record_index, version):
-    rec = BaseSample()
-    rec.record_name = record_id
-    rec.record_index = record_index
-    rec.version = version
-
-    data_set = context.data_source.get_data_set(data_set_id, context.data_set)
-    context.data_source.save_one(BaseSample, rec, data_set)
-
-    return rec.id_
 
 
 class TestTemporalMongoDataSource(unittest.TestCase):
@@ -125,15 +35,15 @@ class TestTemporalMongoDataSource(unittest.TestCase):
 
         with TemporalMongoUnitTestContext() as context:
 
-            save_basic_data(context)
+            self.save_basic_data(context)
 
             key_a0 = 'BaseSample=A;0'
             key_b0 = 'BaseSample=B;0'
 
-            self.assertEqual('Found, type = BaseSample', verify_load(context, 'DataSet0', key_a0))
-            self.assertEqual('Found, type = BaseSample', verify_load(context, 'DataSet1', key_a0))
-            self.assertEqual('Not found', verify_load(context, 'DataSet0', key_b0))
-            self.assertEqual('Found, type = DerivedSample', verify_load(context, 'DataSet1', key_b0))
+            self.assertEqual('Found, type = BaseSample', self.verify_load(context, 'DataSet0', key_a0))
+            self.assertEqual('Found, type = BaseSample', self.verify_load(context, 'DataSet1', key_a0))
+            self.assertEqual('Not found', self.verify_load(context, 'DataSet0', key_b0))
+            self.assertEqual('Found, type = DerivedSample', self.verify_load(context, 'DataSet1', key_b0))
 
     def test_multiple_data_set_query(self):
         """Test working with multiple datasets."""
@@ -144,48 +54,48 @@ class TestTemporalMongoDataSource(unittest.TestCase):
             data_set0 = context.data_source.create_data_set('DataSet0', context.data_set)
 
             # Create initial version of the records
-            save_minimal_record(context, 'DataSet0', 'A', 0, 0)
-            save_minimal_record(context, 'DataSet0', 'B', 1, 0)
-            save_minimal_record(context, 'DataSet0', 'A', 2, 0)
-            save_minimal_record(context, 'DataSet0', 'B', 3, 0)
+            self.save_minimal_record(context, 'DataSet0', 'A', 0, 0)
+            self.save_minimal_record(context, 'DataSet0', 'B', 1, 0)
+            self.save_minimal_record(context, 'DataSet0', 'A', 2, 0)
+            self.save_minimal_record(context, 'DataSet0', 'B', 3, 0)
 
             # Create second version of some records
-            save_minimal_record(context, 'DataSet0', 'A', 0, 1)
-            save_minimal_record(context, 'DataSet0', 'B', 1, 1)
-            save_minimal_record(context, 'DataSet0', 'A', 2, 1)
-            save_minimal_record(context, 'DataSet0', 'B', 3, 1)
+            self.save_minimal_record(context, 'DataSet0', 'A', 0, 1)
+            self.save_minimal_record(context, 'DataSet0', 'B', 1, 1)
+            self.save_minimal_record(context, 'DataSet0', 'A', 2, 1)
+            self.save_minimal_record(context, 'DataSet0', 'B', 3, 1)
 
             # Create third version of even fewer records
-            save_minimal_record(context, 'DataSet0', 'A', 0, 2)
-            save_minimal_record(context, 'DataSet0', 'B', 1, 2)
-            save_minimal_record(context, 'DataSet0', 'A', 2, 2)
-            save_minimal_record(context, 'DataSet0', 'B', 3, 2)
+            self.save_minimal_record(context, 'DataSet0', 'A', 0, 2)
+            self.save_minimal_record(context, 'DataSet0', 'B', 1, 2)
+            self.save_minimal_record(context, 'DataSet0', 'A', 2, 2)
+            self.save_minimal_record(context, 'DataSet0', 'B', 3, 2)
 
             # Same in DataSet1
             data_set1 = context.data_source.create_data_set("DataSet1", context.data_set, [data_set0])
 
             # Create initial version of the records
-            save_minimal_record(context, "DataSet1", "A", 4, 0)
-            save_minimal_record(context, "DataSet1", "B", 5, 0)
-            save_minimal_record(context, "DataSet1", "A", 6, 0)
-            save_minimal_record(context, "DataSet1", "B", 7, 0)
+            self.save_minimal_record(context, "DataSet1", "A", 4, 0)
+            self.save_minimal_record(context, "DataSet1", "B", 5, 0)
+            self.save_minimal_record(context, "DataSet1", "A", 6, 0)
+            self.save_minimal_record(context, "DataSet1", "B", 7, 0)
 
             # Create second version of some records
-            save_minimal_record(context, "DataSet1", "A", 4, 1)
-            save_minimal_record(context, "DataSet1", "B", 5, 1)
-            save_minimal_record(context, "DataSet1", "A", 6, 1)
-            save_minimal_record(context, "DataSet1", "B", 7, 1)
+            self.save_minimal_record(context, "DataSet1", "A", 4, 1)
+            self.save_minimal_record(context, "DataSet1", "B", 5, 1)
+            self.save_minimal_record(context, "DataSet1", "A", 6, 1)
+            self.save_minimal_record(context, "DataSet1", "B", 7, 1)
 
             # Next in DataSet2
             data_set2 = context.data_source.create_data_set("DataSet2", context.data_set, [data_set0])
-            save_minimal_record(context, "DataSet2", "A", 8, 0)
-            save_minimal_record(context, "DataSet2", "B", 9, 0)
+            self.save_minimal_record(context, "DataSet2", "A", 8, 0)
+            self.save_minimal_record(context, "DataSet2", "B", 9, 0)
 
             # Next in DataSet3
             data_set3 = context.data_source.create_data_set("DataSet3", context.data_set,
                                                             [data_set0, data_set1, data_set2])
-            save_minimal_record(context, "DataSet3", "A", 10, 0)
-            save_minimal_record(context, "DataSet3", "B", 11, 0)
+            self.save_minimal_record(context, "DataSet3", "A", 10, 0)
+            self.save_minimal_record(context, "DataSet3", "B", 11, 0)
 
             query = context.data_source.get_query(BaseSample, data_set3) \
                 .where({'record_name': 'B'}) \
@@ -216,6 +126,103 @@ class TestTemporalMongoDataSource(unittest.TestCase):
             # Log should not contain warnings.
             # TODO: fix logging
             # self.assertTrue(str(context.log) == '')
+
+    def save_base_record(self, context: Context, data_set_id, record_id, record_index) -> ObjectId:
+        """Save base record."""
+
+        rec = BaseSample()
+        rec.record_name = record_id
+        rec.record_index = record_index
+        rec.double_element = 100.0
+        rec.local_date_element = LocalDate.from_fields(2003, 5, 1)
+        rec.local_time_element = LocalTime.from_fields(10, 15, 30)  # 10:15:30
+        rec.local_minute_element = LocalMinute.from_fields(10, 15)  # 10:15
+        rec.local_date_time_element = LocalDateTime.from_fields(2003, 5, 1, 10, 15)  # 2003-05-01T10:15:00
+        rec.enum_value = EnumSample.EnumValue2
+
+        data_set = context.data_source.get_data_set(data_set_id, context.data_set)
+        context.data_source.save_one(BaseSample, rec, data_set)
+
+        return rec.id_
+
+    def save_derived_record(self, context: Context, data_set_id, record_id, record_index) -> ObjectId:
+        """Save derived record"""
+
+        rec = DerivedSample()
+        rec.record_name = record_id
+        rec.record_index = record_index
+        rec.double_element = 300.
+        rec.local_date_element = LocalDate.from_fields(2003, 5, 1)
+        rec.local_time_element = LocalTime.from_fields(10, 15, 30)  # 10:15:30
+        rec.local_minute_element = LocalMinute.from_fields(10, 15)  # 10:15
+        rec.local_date_time_element = LocalDateTime.from_fields(2003, 5, 1, 10, 15)  # 2003-05-01T10:15:00
+        rec.string_element2 = ''
+        rec.double_element = 200.
+        rec.list_of_string = ['A', 'B', 'C']
+
+        rec.list_of_double = [1.0, 2.0, 3.0]
+        rec.list_of_nullable_double = [10.0, None, 30.0]
+
+        # Data element
+        rec.data_element = ElementSample()
+        rec.data_element.double_element3 = 1.0
+        rec.data_element.string_element3 = 'AA'
+
+        # Data element list
+
+        element_list0 = ElementSample()
+        element_list0.double_element3 = 1.0
+        element_list0.string_element3 = "A0"
+        element_list1 = ElementSample()
+        element_list1.double_element3 = 2.0
+        element_list1.string_element3 = "A1"
+        rec.data_element_list = [element_list0, element_list1]
+
+        # Key element
+        rec.key_element = 'BB;2'
+
+        # Key element list
+        rec.key_element_list = ['B0;3', 'B1;4']
+
+        data_set = context.data_source.get_data_set(data_set_id, context.data_set)
+        context.data_source.save_one(DerivedSample, rec, data_set)
+        return rec.id_
+
+    def save_basic_data(self, context: Context) -> None:
+        """Two datasets and two objects, one base and one derived."""
+
+        data_set0 = context.data_source.create_data_set('DataSet0', context.data_set)
+        self.save_base_record(context, 'DataSet0', 'A', 0)
+
+        context.data_source.create_data_set('DataSet1', context.data_set, [data_set0])
+        self.save_derived_record(context, 'DataSet1', 'B', 0)
+
+    def verify_load(self, context: Context, data_set_id, key) -> str:
+        """Load the object and verify the outcome."""
+
+        data_set = context.data_source.get_data_set(data_set_id, context.data_set)
+        record = context.data_source.load_or_null_by_key(BaseSample, key, data_set)
+
+        if record is None:
+            return 'Not found'
+        else:
+            if record.to_key() != key:
+                return 'Found, key mismatch.'
+            else:
+                return f'Found, type = {type(record).__name__}'
+
+    def save_minimal_record(self, context: Context, data_set_id, record_id, record_index, version):
+        """Save record with minimal data for testing how the records are found."""
+
+        rec = BaseSample()
+        rec.record_name = record_id
+        rec.record_index = record_index
+        rec.version = version
+
+        data_set = context.data_source.get_data_set(data_set_id, context.data_set)
+        context.data_source.save_one(BaseSample, rec, data_set)
+
+        return rec.id_
 
 
 if __name__ == "__main__":
